7ab877056b5c25312cd76f81c3693251beeee243
NAanhquan <quannahe173363@fpt.edu.vn>
2026-06-10 20:12:56 +0700

    Merge branch main

--- src/main/java/com/bakeryzone/utils/DBContext.java
+++ src/main/java/com/bakeryzone/utils/DBContext.java
@@ -13,9 +13,9 @@ import java.util.logging.Logger;
 public class DBContext {
 
     public static Connection getJDBCConnection() {
-        String url = "jdbc:mysql://localhost:3306/bakery";
+        String url = "jdbc:mysql://localhost:3306/BakeryManagement";
         String user = "root"; 
-        String password = "123456";
+        String password = "1234";
         
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
